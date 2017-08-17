e6df1119b1432655614b5d5be9bcd4c316e4cba5
Luca Stefani <luca020400@lineageos.org>
2017-08-17 22:22:13 +0200

    core: Include Potato pathmap
    Change-Id: Ie25dee383cc5bc9bb6390cff2cb2460d526d80b6
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- core/pathmap.mk
+++ core/pathmap.mk
@@ -93,3 +93,7 @@ FRAMEWORKS_BASE_SUBDIRS := \
 #
 FRAMEWORKS_BASE_JAVA_SRC_DIRS := \
 	$(addprefix frameworks/base/,$(FRAMEWORKS_BASE_SUBDIRS))
+
+ifneq ($(POTATO_BUILD),)
+-include $(TOPDIR)vendor/potato/build/core/pathmap.mk
+endif
