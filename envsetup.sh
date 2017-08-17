db4e0f0fd74d7a9a8aaca3be11bbe082f765977a
Luca Stefani <luca020400@lineageos.org>
2017-08-17 21:21:23 +0200

    build: Add Potato build support
    core: Move lineage sepolicy to device/
    
    [@AgentFabulous - POSP]
    - Don't include custom sepolicy dirs for now
    - Forward port to P and use AndroidProducts.mk much like AOSP and now
      LineageOS.
    
    Change-Id: Iab8b2ef8b644a7bea10bfd28b99d9e69539b1357
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- envsetup.sh
+++ envsetup.sh
@@ -144,6 +144,14 @@ function check_product()
         echo "Couldn't locate the top of the tree.  Try setting TOP." >&2
         return
     fi
+    if (echo -n $1 | grep -q -e "^potato_") ; then
+        POTATO_BUILD=$(echo -n $1 | sed -e 's/^potato_//g')
+        export BUILD_NUMBER=$( (date +%s%N ; echo $POTATO_BUILD; hostname) | openssl sha1 | sed -e 's/.*=//g; s/ //g' | cut -c1-10 )
+    else
+        POTATO_BUILD=
+    fi
+    export POTATO_BUILD
+
         TARGET_PRODUCT=$1 \
         TARGET_BUILD_VARIANT= \
         TARGET_BUILD_TYPE= \
@@ -653,6 +661,8 @@ function lunch()
         echo "Invalid lunch combo: $selection"
         return 1
     fi
+
+    check_product $product
 
     TARGET_PRODUCT=$product \
     TARGET_BUILD_VARIANT=$variant \
