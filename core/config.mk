fc6d90f45c398d5f61f6bd311d24819ad9334b0e
Kshitij Gupta <kshitijgm@gmail.com>
2018-04-14 17:08:03 +0000

    build: Enable potato sepolicy
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -1183,7 +1183,7 @@ endif
 ifneq ($(POTATO_BUILD),)
 ## We need to be sure the global selinux policies are included
 ## last, to avoid accidental resetting by device configs
-# $(eval include device/potato/sepolicy/common/sepolicy.mk)
+$(eval include device/potato/sepolicy/common/sepolicy.mk)
 
 # Include any vendor specific config.mk file
 -include $(TOPDIR)vendor/*/build/core/config.mk
